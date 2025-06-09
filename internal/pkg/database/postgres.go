a3ff1a3b92de47988764249eef492e503dafd937
Osas997 <larasatyt83@gmail.com>
2025-06-10 00:29:40 +0700

    fix config db

--- internal/pkg/database/postgres.go
+++ internal/pkg/database/postgres.go
@@ -13,7 +13,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%q dbname=%s port=%s sslmode=%s",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
